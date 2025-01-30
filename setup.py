d677cfde1131432a6db1b7f8cd71961b0412837d
Michael Hütter <125815831+michael-huetter@users.noreply.github.com>
2025-01-30 13:09:28 +0100

    change layout

--- setup.py
+++ setup.py
@@ -3,24 +3,26 @@ import pybind11
 
 ext_modules = [
     Extension(
-        'PIMC',
-        ['binding.cpp', 'mcmc.cpp', 'Beads.cpp', 'Energy.cpp', 'Potential.cpp'],
+        "PIMC",
+        ["src/binding.cpp", "src/mcmc.cpp", "src/Beads.cpp", "src/Energy.cpp", "src/Potential.cpp"],
         include_dirs=[
             pybind11.get_include(),
-            '/opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3',
-            '/opt/homebrew/Cellar/autodiff/1.1.2', 
+            "/opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3",
+            "/opt/homebrew/Cellar/autodiff/1.1.2", 
+            "/include"
+            
         ],
-        language='c++',
-        extra_compile_args = ['-O3', '-std=c++17', '-Wall', '-Wextra'], 
+        language="c++",
+        extra_compile_args = ["-O3", "-std=c++17", "-Wall", "-Wextra"], 
     ),
 ]
 
 setup(
-    name='PIMC',
-    version='1.2',
-    author='Michael Hütter',
-    author_email='',
-    description='PIMC code',
+    name="PIMC",
+    version="1.2",
+    author="Michael Hütter",
+    author_email="",
+    description="PIMC code",
     ext_modules=ext_modules,
-    install_requires=['pybind11'],
+    install_requires=["pybind11"],
 )
